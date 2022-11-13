64eb87612af515a2489869ffcc98e5aa69961851
Brian-Machiestay <brianmachiestay@gmail.com>
2022-11-13 10:42:00 +0000

    view function for hbnb

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" starts a flask web application"""
+from flask import Flask
+app = Flask('web_flask')
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """returns hbnb message"""
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """view function for /hbnb"""
+    return 'HBNB'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
