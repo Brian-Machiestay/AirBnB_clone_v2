ef8ca6f0ae835e48caa2e478f25cf3cfc65d7588
Brian-Machiestay <brianmachiestay@gmail.com>
2022-11-13 10:20:19 +0000

    my first flask app

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" starts a flask web application"""
+from flask import Flask
+app = Flask('web_flask')
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """returns hbnb message"""
+    return 'Hello HBNB!'
+app.run(host='0.0.0.0')
