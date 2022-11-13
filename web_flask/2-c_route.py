fc5dbef176f8800baa73c854468bbead99b30589
Brian-Machiestay <brianmachiestay@gmail.com>
2022-11-13 10:50:06 +0000

    dynamic view function for an endpoint

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" starts a flask web application"""
+from flask import Flask
+app = Flask('web_flask')
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """returns hbnb message"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """view function for /hbnb"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """dynamic view function for /c/text"""
+    modified_text = text.replace("_", " ")
+    return "C {}".format(modified_text)
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
